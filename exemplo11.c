52b883e554da9ca6661ea66c09d69b41d567a7df
Marcello <mvtedardi@gmail.com>
2018-04-07 16:53:27 -0300

    comentarios no codigo da parte 2

--- exemplo11.c
+++ exemplo11.c
@@ -1,36 +1,68 @@
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <stdio.h>
+/*
+** Este exemplo, correspondente ao 3.9b do capitulo 03 do livro do Alex Vrenios, recebe os pacotes TCP do cliente
+** e os disponibiliza no terminal. 
+*/
 
+// contem definicoes e estruturas para a comunicacao IP
+#include <netinet/in.h>
+// contem as implementacoes das estruturas dos sockets
+#include <sys/socket.h>
+// contem as definicoes do tipo porta e ip que sao descritas em <netinet/in.h>.
+#include <netdb.h>
+// padrao para inputs e outputs
+#include <stdio.h>
+// ADICIONADA pela funcao exit
 #include <stdlib.h>
+// usada por causa do metodo memset
 #include <string.h>
+// ADICIONADA por conhecer o metodo close() de sockets
 #include <unistd.h>
 
+/* As portas são definidas em 16bits, ou seja, existem as portas de 0 a 65535.
+** Entretanto, as portas 0 ate 1999 sao reservadas para processos conhecidos e padronizados.
+** Aconselha-se quando criar o processo, escolher uma porta entre 2000 a 65535.
+*/
 #define PORT 4321
 
 int main(void)
-/*
-** Listing 3.9b.c - prints TCP packets from a remote client
-*/
 {
+
+/*
+** ***************************************************************************************************************
+** Nesta primeira parte do codigo, cria-se as estruturas necessarias para receber os pacotes do cliente pelo servidor.
+** ***************************************************************************************************************
+*/
+
+	// estrutura do socket conforme <sys/socket.h>, definida na implementacao como:  
+		/*
+		** struct sockaddr_in
+		** {
+  		** short   sin_family; precisa ser AF_INET
+  		** u_short sin_port;
+  		** struct  in_addr sin_addr;
+  		** char    sin_zero[8];
+		**	};
+		*/
 	struct sockaddr_in sock;
+	memset((char*) &sock, 0, sizeof(sock));
 	int len = sizeof(sock);
 	struct hostent *hp;
 	int port = PORT;
 	int acc, soc;
 	char buffer[80];
 
-	/* create a server socket to accept a connection */
+	/* Cria o socket para suportar conexão usando IPv4 */
 	if((acc = socket(AF_INET, SOCK_STREAM, 0)) <0)
 	{
 		perror("server: socket()");
 		exit(-1);
 	}
+
 	memset((char *) &sock, 0, sizeof(sock));
 	sock.sin_addr.s_addr = htonl(INADDR_ANY);
 	sock.sin_port = htons(port);
 	sock.sin_family = AF_INET;
+
 	if(bind(acc, (struct sockaddr *) &sock, len) <0)
 	{
 		perror("server:bind()");
@@ -38,13 +70,15 @@ int main(void)
 		exit(-1);
 	}
 
-	/* await connection */
+	/* aguarda a conexão */
 	if(listen(acc, 5) <0)
 	{
 		perror("server: listen()");
 		close(acc);
 		exit(-1);
 	}
+
+	/* verifica o aceite da conexão */
 	if((soc = accept(acc, (struct sockaddr *) &sock, &len)) <0)
 	{
 
@@ -54,11 +88,21 @@ int main(void)
 	}
 	close(acc);
 
-	/* echo messages received from client back to user */
+
+/*
+** ***************************************************************************************************************
+** Nesta segunda parte do código, a conexão do TCP já foi executada. Imprime os pacotes recebidos
+** do(s) cliente(s).
+** ***************************************************************************************************************
+*/
+
+	// o while 1 cria um loop que termian apenas caso a condicional do if seja TRUE.
 	while(1)
 	{
 		recv(soc, buffer, sizeof(buffer), 0);
 		printf("Server: %s", buffer);
+		// if(strncmp(buffer "EXIT", 4) == 0) /* exit request */ - original sem virgula entre buffer e exit
+		// Aqui, a condicional permite o servidr finalizar a sua comunicação caso o cliente envie a palavra EXIT
 		if(strncmp(buffer, "EXIT", 4) == 0) /* exit request */
 		{
 			puts("Bye!");
@@ -66,5 +110,6 @@ int main(void)
 			break;
 		}
 	}
+
 	return(0);
 }
--- exemplo9.c
+++ exemplo9.c
@@ -1,21 +1,43 @@
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
+/*
+** Este exemplo, correspondente ao 3.8b do capitulo 03 do livro do Alex Vrenios, recebe os pacotes UDP do cliente
+** e os disponibiliza no terminal. 
+*/
 
-// Adicionad string devido função memset
+// contem definicoes e estruturas para a comunicacao IP
+#include <netinet/in.h>
+// contem as implementacoes das estruturas dos sockets
+#include <sys/socket.h>
+// contem as definicoes do tipo porta e ip que sao descritas em <netinet/in.h>.
+#include <netdb.h>
+// padrao para inputs e outputs
+#include <stdio.h>
+// ADICIONADA pela funcao exit
+#include <stdlib.h>
+// usada por causa do metodo memset
 #include <string.h>
-// Adicionad devido função close()
+// ADICIONADA por conhecer o metodo close() de sockets
 #include <unistd.h>
 
+/* As portas são definidas em 16bits, ou seja, existem as portas de 0 a 65535.
+** Entretanto, as portas 0 ate 1999 sao reservadas para processos conhecidos e padronizados.
+** Aconselha-se quando criar o processo, escolher uma porta entre 2000 a 65535.
+*/
 #define PORT 5678
 
 int main(void)
+
 /*
-** Listing 3.8b.c- prints UDP datagrams from a remote client
+** ***************************************************************************************************************
+** Nesta primeira parte do codigo, cria-se as estruturas necessarias para receber os pacotes do cliente pelo servidor.
+** ***************************************************************************************************************
 */
+
 {
+
+/*
+** Inicialmente, ele inicia o código declarando o socket da biblioteca <sys/socket.h>
+** Uma comunicação UDP precisa apenas da porta e do IP para ser estabelecida.
+*/
 	struct sockaddr_in sock;
 	int len = sizeof(sock);
 	struct hostent *hp;
@@ -29,8 +51,10 @@ int main(void)
 		perror("server: socket()");
 		exit(-1);
 	}
-	memset((char*) &sock, 0, sizeof(sock));
+	memset((char*) &sock, 0, sizeof(sock)); /* Esta linha de codigo nao entendi */
 	// sock.sin_addr.s_addr = htonl(INNADR_ANY);-- original, provavelmente tem typo
+
+
 	sock.sin_addr.s_addr = htonl(INADDR_ANY);
 	sock.sin_port = htons(port);
 	sock.sin_family = AF_INET;
@@ -40,18 +64,33 @@ int main(void)
 		close(fd);
 		exit(-1);
 	}
-	
+
+
+/*
+** ***************************************************************************************************************
+** Nesta segunda parte do código, a conexão do UDP já foi executada. Imprime os pacotes recebidos
+** do(s) cliente(s).
+** ***************************************************************************************************************
+*/
 	/* echo messages received from client back to user */
+	/*
+	** No bloco a seguir, há um loop que se encessará apenas com a mensagem "EXIT".
+	** Neste loop, o servidor está procurando possíveis mensagens de sockets UDP. Ele usa um buffer
+	** para guardar essas mensagens.
+	** Aqui pode ser adicionada qualquer intervenção o processamento mais simples do servidor para o cliente.
+	*/
 	while(1)
 	{
 		recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr *) &sock, &len);
 		printf("Server: %s", buffer);
+		
 		if(strncmp(buffer, "EXIT", 4) == 0) /* exit request */
 		{
 			puts("Bye!");
 			close(fd);
 			break;
 		}
+		
 	}
 	return(0);
 }
