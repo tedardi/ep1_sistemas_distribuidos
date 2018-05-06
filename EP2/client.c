da91b57cfe9a0a8677807b291b7708ff5e5a44fe
Antonio Carlos <baptistaacbc@hotmail.com>
2018-05-06 20:43:04 -0300

    Comentado código do cliente

--- EP2/client.c
+++ EP2/client.c
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
   // Timer value before/after
   struct timeval before, after;
 
-  // Socket structure
+  // Socket structure: guarda o tipo de familia do end (IPV4), numero da porta, endereco IP e alguns complementos
   struct sockaddr_in sock;
 
-  // Structure for IP address
+  // Structure for IP address. Guarda informacao de um determinado host, como o nome, o IPV4
   struct hostent *hp;
 
   double random;
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 
   char buf[80], *p;
 
-  // Seed rand()
+  // Seed rand() - cria uma seed para os números aleatórios
   srand((unsigned int) getpid());
 
   // Operands are remote HOST name and IAT (Inter-arrival Time)
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
     exit(-1);
   }
 
-  // uSeconds part of inter-arrival
+  // uSeconds part of inter-arrival - strchr procura pelo char '.',
   if((p=strchr(argv[2], '.')) != NULL)
   {
     // End whole number at decimal
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
     p[6] = '\0';
     usec = atol(p);
   }
-  // Seconds part of IAT
+  // Seconds part of IAT - recupera as partes em segundo do IAT
   secs = atol(argv[2]);
 
   // LOOP: send and receive NUM packets
@@ -90,25 +90,41 @@ int main(int argc, char *argv[])
     jj = (int) ((double) (5.0) * random) + 1;
     if(jj == 6)
       jj = 5;
-    sprintf(buf, "/tmp/sample.txt %d", jj);
+    sprintf(buf, "./sample.txt %d", jj);
 
+
+    // Cria um filho
     if((pids[kk++]=fork()) == 0)
     {
+
       // set up socket info for connect
+      // SOCK_STREAM = TCP | SOCK_DGRAM = UDP
+      // Configura protocolo (ultimo parametro) como 0 para selecionar default do S.O
       fd = socket(AF_INET, SOCK_STREAM, 0);
       memset((char *) &sock, 0, sizeof(sock));
       hp = gethostbyname(argv[1]);
+
+      // Copia end ip do HOST para endereço ip do Socket
       memcpy(&sock.sin_addr, hp->h_addr, hp->h_length);
       sock.sin_family = hp->h_addrtype;
+
+      // Guarda porta em 16bits dentro do socket
       sock.sin_port = htons(PORT);
+
+      // Definindo opções do socket, dentro do file descriptor do socket, definimos a nivel de socket, reutilizando endereços locais caso necessário
       setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt));
 
-      // Connect to server
+      // Connect to server - fd especifica as opções do socket (IPV4, TCP, Re-utilizar end locais, etc), sock define a struct do socket com o IP atual, porta, etc.
       connect(fd, (struct sockaddr *) &sock, sizeof(sock));
-      send(fd, buf, strlen(buf) + 1, 0);
+
+      // Envia do socket (descriptor) o buf com o length buf+1 sem flags
+      int bits_enviados = send(fd, buf, strlen(buf) + 1, 0);
+      printf("\nBits enviados %d\n", bits_enviados);
 
       // Limpa buffer
       buf[0] = 0;
+
+      // Recebe mensagem do socket fd, utilizando o buffer buf
       recv(fd, buf, sizeof(buf), 0);
 
       // Print out response to our query
